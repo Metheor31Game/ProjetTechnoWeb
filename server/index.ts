7c7f79bb1aadd874bf46233641c534af89966e2b
matheo <matheo>
2024-11-14 16:17:28 +0100

    reglage

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { engine } from "express-handlebars";
 import path from "path";
-import { assertType, expectTypeOf, test } from 'vitest'
+import { assertType, expectTypeOf, test } from "vitest";
 import { initialiseDatabase } from "./bdd";
 
 //const exphbs = require("express-handlebars");
@@ -21,32 +21,29 @@ app.get("/", (req: Request, res: Response) => {
     title: "Bienvenue",
     description: "Ceci est un site internet qui utilise handlebars",
   });
-
 });
 
 //Route pour se connecter
 app.get("/connection", (req: Request, res: Response) => {
-  res.render("connection", {
-
-  })
-})
+  res.render("connection", {});
+});
 
 //Route pour s'inscrire
-app.get("/inscription", (req:Request, res:Response) => {
-  res.render("inscription", {
-    
-  })
-})
+app.get("/inscription", (req: Request, res: Response) => {
+  res.render("inscription", {});
+});
 
 //Lancer la base de donnée
-initialiseDatabase().then(() => {
-  //lancer le serveur du site
-  app.listen(port, () => {
-      console.log(`serveur sur écoute : http://localhost:${port}`)
+initialiseDatabase()
+  .then(() => {
+    //lancer le serveur du site
+    app.listen(port, () => {
+      console.log(`serveur sur écoute : http://localhost:${port}`);
+    });
   })
-}).catch(err => {
-  console.error("Erreur lors de l'initialisation de la DATABASE : ", err)
-})
+  .catch((err) => {
+    console.error("Erreur lors de l'initialisation de la DATABASE : ", err);
+  });
 
 /*
 if (import.meta.hot) {
@@ -57,5 +54,3 @@ if (import.meta.hot) {
   
 }
 */
-
-
