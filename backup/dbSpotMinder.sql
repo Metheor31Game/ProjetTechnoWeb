bd0504fbc2c9770480497717355b5d52549ae3e0
clement <clement>
2024-12-10 15:53:59 +0100

    backup maj

--- backup/dbSpotMinder.sql
+++ backup/dbSpotMinder.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : db
--- Généré le : lun. 09 déc. 2024 à 21:12
--- Version du serveur : 8.0.40
+-- Généré le : mar. 10 déc. 2024 à 14:52
+-- Version du serveur : 8.0.39
 -- Version de PHP : 8.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -29,11 +29,33 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `annonce` (
   `id` int NOT NULL,
+  `id_dashboard` int NOT NULL,
   `titre` varchar(60) NOT NULL,
   `date` varchar(20) NOT NULL,
   `adresse` varchar(80) NOT NULL,
   `description` varchar(220) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `dashboard`
+--
+
+CREATE TABLE `dashboard` (
+  `id_dashboard` int NOT NULL,
+  `pseudo_user` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `dashboard`
+--
+
+INSERT INTO `dashboard` (`id_dashboard`, `pseudo_user`) VALUES
+(1, 'Armand66'),
+(2, 'Clem12'),
+(3, 'Matheo31'),
+(4, 'Thibault81');
 
 -- --------------------------------------------------------
 
@@ -64,12 +86,59 @@ INSERT INTO `utilisateur` (`pseudo`, `nom`, `prenom`, `mail`, `mdp`) VALUES
 --
 
 --
+-- Index pour la table `annonce`
+--
+ALTER TABLE `annonce`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_dashboard` (`id_dashboard`);
+
+--
+-- Index pour la table `dashboard`
+--
+ALTER TABLE `dashboard`
+  ADD PRIMARY KEY (`id_dashboard`),
+  ADD KEY `FK_USER_DASHBOARD` (`pseudo_user`);
+
+--
 -- Index pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`pseudo`),
   ADD UNIQUE KEY `pseudo` (`pseudo`),
   ADD UNIQUE KEY `mail` (`mail`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `annonce`
+--
+ALTER TABLE `annonce`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `dashboard`
+--
+ALTER TABLE `dashboard`
+  MODIFY `id_dashboard` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `annonce`
+--
+ALTER TABLE `annonce`
+  ADD CONSTRAINT `FK_ANNONCE_DASHBOARD` FOREIGN KEY (`id_dashboard`) REFERENCES `dashboard` (`id_dashboard`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `NOT NULL` FOREIGN KEY (`id_dashboard`) REFERENCES `dashboard` (`id_dashboard`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Contraintes pour la table `dashboard`
+--
+ALTER TABLE `dashboard`
+  ADD CONSTRAINT `FK_USER_DASHBOARD` FOREIGN KEY (`pseudo_user`) REFERENCES `utilisateur` (`pseudo`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
