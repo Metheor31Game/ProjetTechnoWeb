0b942437f86be0207145379746296e8cf0324fe9
matheo <matheo>
2024-12-10 15:09:32 +0100

    start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,11 @@
 npm install
 echo "npm install : ok"
 
+cd ./docker/
+mkdir mysql && mkdir www
+chmod 777 mysql/ && chmod 777 www/
 docker compose up -d
 echo "docker compose : ok"
+npm run dev
 
-docker exec -i docker-myadmin-1 mysql -u root -p adminspotminder dbSpotMinder < ../backup/dbSpotMinder.sql
+# docker exec -i docker-myadmin-1 mysql -u root -p adminspotminder dbSpotMinder < ../backup/dbSpotMinder.sql
